df6605c2995494e85f7709ccd39a6643e1ab29a4
bbpak <brianbpak@gmail.com>
2019-03-14 14:51:52 -0700

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,35 @@
 class LandmarksController < ApplicationController
   # add controller methods
+  set :views, 'app/views/landmarks'
+
+  # add controller methods
+  get '/landmarks' do
+    erb :index
+  end
+
+  get '/landmarks/new' do
+    erb :new
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :show
+  end
+
+  get '/landmarks/:id/edit' do
+    #binding.pry
+    @landmark = Landmark.find(params[:id])
+    erb :edit
+  end
+
+  patch '/landmarks/:id' do
+    landmark = Landmark.find(params[:id])
+    landmark.update(params[:landmark])
+    redirect "/landmarks/#{params[:id]}"
+  end
+
+  post '/landmarks' do
+    Landmark.create(params[:landmark])
+    redirect '/landmarks'
+  end
 end
---
+++ db/migrate/02_create_figures.rb
@@ -0,0 +1,7 @@
+class CreateFigures < ActiveRecord::Migration
+
+  create_table :figures do |t|
+    t.string :name
+  end
+
+end
