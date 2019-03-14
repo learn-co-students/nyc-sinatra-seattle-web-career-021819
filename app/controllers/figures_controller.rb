dbb621f7d9ee61e80fb7a086bd4910ba23040e48
github username <github email address>
2019-03-14 13:31:23 -0700

    major changes

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,6 +1,13 @@
 class FiguresController < ApplicationController
+  # set :views, 'app/views/figures'
+
   get "/figures" do
     @figures = Figure.all
-    erb :index
+    erb :"/figures/index"
   end
+
+  get "/figures/new" do
+    erb :"/figures/new"
+  end
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,23 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+  # set :views, 'app/views/landmarks'
+  get '/landmarks' do
+  @landmarks = Landmark.all
+  erb :"/landmarks/index"
+  end
+
+  get '/landmarks/new' do
+
+    erb :"/landmarks/new"
+  end
+
+  post "/landmarks/:id" do
+    landmark = Landmark.create(params)
+    redirect "/landmarks/#{landmark.id}"
+  end
+
+  get "/landmarks/:id" do
+    @landmark = Landmark.find(params[:id])
+    erb :"/landmarks/show"
+  end
+
 end
